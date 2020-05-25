1cc7e674c1a22770ecc3d1ec7a4003a3e5968ec0
sathishs007 <sathishs.scube@gmail.com>
2020-05-25 12:55:39 +0530

    Updates

---
+++ precache-manifest.c169c67eb350c4e418301807248917f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b2fa9d39c3b2b813b02f550203ae7b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "d7447eb34e8221a56c41",
+    "url": "/react-deploy/static/css/main.5c15533e.chunk.css"
+  },
+  {
+    "revision": "169db92b2963a491015e",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7447eb34e8221a56c41",
+    "url": "/react-deploy/static/js/main.06bc0cc7.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  }
+]);
